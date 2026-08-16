f52cd509cf38b1fe635bc4be9dceffe6afe49aa2
Panpan-800 <paula800156@gmail.com>
2026-08-16 03:16:13 -0600

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post("/chat", async (req, res) => {
     }
 
     const result = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
+      model: "gemini-3.7-flash",
       contents: message,
       config: {
         systemInstruction:
