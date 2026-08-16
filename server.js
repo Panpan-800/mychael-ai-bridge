0b6f750aecb69acc68fd14dd7598e31cb828d3e8
Panpan-800 <paula800156@gmail.com>
2026-08-16 02:51:54 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const app = express();
 
 app.use(express.json({ limit: "64kb" }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 const PORT = process.env.PORT || 10000;
 
 app.get("/", (req, res) => {
